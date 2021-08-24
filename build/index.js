49b48c93d55d2d3467ba6361cd10380d86298979
PRASAD P <prasad.p@changecx.com>
2021-08-24 13:15:02 +0530

    sample code done.

--- build/index.js
+++ build/index.js
@@ -1,4 +1,10 @@
-export function testMethod(param) {
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function testMethod(param) {
     return "Hello " + param;
 }
+
+exports.testMethod = testMethod;
 //# sourceMappingURL=index.js.map
